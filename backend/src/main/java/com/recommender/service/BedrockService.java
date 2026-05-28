ca1bc4596fae161ebe28b79b691ddfd9cc91c9d9
William Makori <williammakori99@yahoo.com>
2026-05-28 07:14:03 -0700

    No minor changes

---
+++ backend/src/main/java/com/recommender/service/BedrockService.java
@@ -0,0 +1,8 @@
+package com.recommender.service;
+
+import com.recommender.model.Recommendation;
+import com.recommender.model.Recommendation.RecommendedItem;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper; 
+import software.amazon.awssdk.core.SdkBytes; 
+
